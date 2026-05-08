e5e5e66ac8276ab26fd10a4654160b10e954170e
Chumleee <88804191+Chumleee@users.noreply.github.com>
2026-05-07 22:58:11 -0600

    mensaje de error en login con credenciales incorrectas

--- universe_hub/universe_hub/settings.py
+++ universe_hub/universe_hub/settings.py
@@ -141,7 +141,7 @@ AUTH_USER_MODEL = 'users.StudentUser'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
