99ce6e0ee523e951c8ce5b268f246ebcae129d87
Chumleee <88804191+Chumleee@users.noreply.github.com>
2026-05-19 11:31:01 -0600

    Update wsgi.py

--- universe_hub/universe_hub/wsgi.py
+++ universe_hub/universe_hub/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for universe_hub project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Agrega el directorio raíz al path de Python para que encuentre 'universe_hub'
+# Esto asume que wsgi.py está dentro de la carpeta 'universe_hub'
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'universe_hub.settings')
 
 application = get_wsgi_application()
+
+# Vercel necesita que la variable se llame 'app' en algunos casos de enrutamiento
+app = application
